874bbf830a7c227253858596c8a6cb2368a18fe0
Joel Blomster <blomsterjoel@gmail.com>
2026-05-02 03:52:59 +0300

    Implement product modification and deletion features; update README and TODO

--- main.py
+++ main.py
@@ -1,48 +1,33 @@
 from database import create_table
-from functions import add_item, list_items, list_commands, available_commands, del_item
+from functions import add_item, list_items, list_commands, available_commands, del_item, modify_item
 
 
 def main():
-    
-    # making sure the table exists before program starts
     create_table()
 
     print("Welcome to Pynventory!\n")
-    print("Type \"help\" to see available commands.\n")
+    print('Type "help" to see available commands.\n')
 
-    # running the program until user chooses exit
     while True:
         command = input("Enter command: ").strip().lower()
 
-        # running add command
         if command == "add":
             add_item()
-
-        # running list command
         elif command == "list":
             list_items()
-
-        # showing available commands
         elif command == "help":
             print(*available_commands, sep=", ")
             print()
-
-        # closing the program
         elif command == "exit":
             print("Exiting Pynventory.")
             break
-
-        # deleting product by ID
         elif command == "del":
             del_item()
-
-        # placeholder for future modify command
         elif command == "modify":
-            print("Modify function not implemented yet.\n")
-
-        # showing help if command is invalid
+            modify_item()
         else:
             list_commands()
 
 
-main()
+if __name__ == "__main__":
+    main()
