91f22ab97ec1f2f94c63239fe773230968813b92
Joel B <64746667+blomma-dev@users.noreply.github.com>
2026-05-11 20:39:42 +0300

    fixing logic that search jumps out of function when wrong option is entered

--- pynventory/db_operations.py
+++ pynventory/db_operations.py
@@ -510,6 +510,7 @@ def search_brand_or_product():
         print(
             "Incorrect option, select either brand or product (or leave empty for both)"
         )
+        search_brand_or_product()
 
     # fetch the actual results from DB
     results = cursor.fetchall()
